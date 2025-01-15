0ebd75bd7a059d38ed49a75ae3693850b2070c01
Issa-WebDev <issawebdev@gmail.com>
2025-01-15 09:38:51 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { FaPlay } from "react-icons/fa";
+
+const App = () => {
+  return (
+    <div>
+      <FaPlay size={40} className="text-violet-700" />
+      <h1 className="text-teal-500 underline">vite + react + tailwind</h1>
+    </div>
+  );
+};
+
+export default App;
